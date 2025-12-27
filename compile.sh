c772d66979178e4ce5b380ad842d4188fad9edcd
KittenAmogus <kittenamogus3@gmail.com>
2025-12-27 19:18:54 +0000

    Hello, World

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
-nasm -f bin src/kernel/kernel.asm -o build/kernel.bin
-nasm -f bin src/bootloader/bootloader.s -o build/boot.bin
+nasm\
+	-f bin\
+	-o build/boot.bin\
+	src/boot/boot.s
 
-ld build/*.bin -o build/kittenos
+qemu-system-x86_64\
+	build/boot.bin\
+	-m 256\
+	-cpu pentium
 
-qemu-system-x86_64 build/kittenos -m 256 -vga std $@
---
+++ src/boot/boot.s
@@ -0,0 +1,57 @@
+bits 16
+
+[org 0x7C00]
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	
+	call clear
+
+	mov si, welcome_message
+	call print
+
+	jmp $
+
+; Constants
+
+welcome_message	db "Hello, World", 00
+
+; Functions
+
+clear:
+	mov ah, 0x07
+	mov al, 0
+	mov bh, 0x07
+	mov cx, 0x0000
+	mov dx, 0x184F
+	int 0x10
+
+	mov ah, 0x02
+	mov bh, 0
+	mov dh, 0
+	mov dl, 0
+	int 0x10
+
+	ret
+
+print:
+	lodsb		; load from si
+	
+	cmp al, 0
+	je .done
+
+	mov ah, 0x0E
+	int 0x10
+	jmp print
+
+	.done:
+		ret
+
+; Stack
+
+
+
+; Boot
+times 510-($-$$) db 0
+dw 0xAA55
+
